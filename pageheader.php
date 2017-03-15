c0707e272b5566bfd3ca046279f07ced61bd3ae0
szhang84 <szhang84@illinois.edu>
2017-03-14 22:47:59 -0500

    Update pageheader.php

--- pageheader.php
+++ pageheader.php
@@ -8,7 +8,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="stack.php">STACKSITY</a>
+                <a class="navbar-brand" href="stack.php">eBaseEXCG</a>
             </div>
             <div id="navbar" class="navbar-collapse collapse">
                 <ul class="nav navbar-nav navbar-right">
